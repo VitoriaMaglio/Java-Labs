9a1275cf697310249c751be6888c0e31a28fe9b3
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-08-07 13:56:44 -0300

    Organizando arquivos.

---
+++ Programming_concepts.java
@@ -0,0 +1,32 @@
+public class Programming_concepts {
+    /*Algoritmo é uma sequência finita de instruções para resolver um problema
+    Automação é o processo de utilizar máquinas para executar o procedimento desejado de forma automática
+     Computador é uma máquina que automatiza a execução de algoritmos.
+     Linguagem de programação são regras léxicas e sintáticas para se escrever o programa
+     IDE: Ambiente Integrado de Desenvolvimento = software para editar e testar.
+     Compilador:software para transformar o código fonte em código objeto.
+     Máquina virtual: software que permite que o programa seja executado. JVM
+     Código fonte é o código escrito em uma linguaguem de programação
+     Compilação é a linguagem que o computador entende: código fonte->código objeto->código executável
+     Interpretação transforma automaticamente o código fonte em executável
+     Abordagem híbrida -> código fonte é compilado, transformado em bytecode que é interpretado por uma máquina virtual.
+     LTS=LONG TERM SUPPORT, não precisa ficar atualizando toda hora
+     Java foi criado na década 1990 pela Sun Microsystems, adquirida pela Oracle Corporation em 2010.
+     Compilação Just in time; JVM máquina virtual do java que executa os sistesmas de java, um compilador gera um código executável específico para cada sistema;
+     o bytecode que é gerado na compilação roda em JVM nos sistemas computacionais.
+     JAVA é uma linguagem orientada a objetos:
+     todos os conceitos devem estar dentro de uma Classe;
+     package são agrupamentos lógicos de classes;
+     módulo são agrupamentos de pacotes relacionados;
+     aplicação é um agrupamento de módulos.
+     Programa em Java só roda com main declarado.
+Estrutura sequencial:
+variáveis são porções de memória RAM que armazenam dados durante a execução de programas.
+declarar var:
+<tipo> <nome> = <valor>;
+int/double/boolean/char/ nomeVar = valor
+char é um caractere Unicode
+String é uma cadeia de caracteres(Classe)
+Entrada de dados ->processamento ->Saída de dados
+     */
+}
